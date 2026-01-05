aa1fc0ef2e2b4032368bce92d8fe9109397a208d
LucasFCustodio <lcustodio2023@fau.edu>
2026-01-05 17:28:04 -0500

    home-personal-professional-contact pages almost done

---
+++ index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import bodyParser from "body-parser";
+import ejs from "ejs";
+import nodemailer from "nodemailer";
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.get("/personal-projects", (req, res) => {
+    res.render("personal.ejs", {
+        pageCss: "/styles/personal-professional.css"
+    });
+});
+
+app.get("/professional-projects", (req, res) => {
+    res.render("professional.ejs", {
+        pageCss: "/styles/personal-professional.css"
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs" , {
+        pageCss: "/styles/about-contact.css"
+    });
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs", {
+        pageCss: "/styles/about-contact.css"
+    });
+});
+
+app.post("/submit", (req, res) => {
+    const name = req.body.fname + " " + req.body.lname;
+    const email = req.body.email;
+    const company = req.body.company;
+    const message = req.body.message;
+    res.render("submit.ejs", {
+        name: name,
+        email: email,
+        company: company,
+        message: message,
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
